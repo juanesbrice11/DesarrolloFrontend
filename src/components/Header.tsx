bace9a260b3bfab1e4ed6b2765d1e288cc26e40d
juanesbrice11 <juanesbrice977@gmail.com>
2024-08-23 17:13:15 -0500

    First commit, base del proyecto en clase

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import '../styles/Header.css'
+export default function Header() {
+  return (
+    <header className="header">
+        This is the header
+    </header>
+  )
+}
---
+++ src/components/Principal.tsx
@@ -0,0 +1,13 @@
+import '../styles/Principal.css'
+export default function Principal() {
+  return (
+    <main className="main">
+        <div className="aside">
+            This is the aside
+        </div>
+        <div className="principal">
+            This is the principal content
+        </div>
+    </main>
+  )
+}
